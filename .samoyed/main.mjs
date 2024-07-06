84cbbfb88d03d4ab414eeb7b1e5d19f0d6aa2ac0
ilk-1 <ilovekfc1024@qq.com>
2024-07-06 11:01:27 +0800

    test: 1

--- .samoyed/main.mjs
+++ .samoyed/main.mjs
@@ -1,79 +1,72 @@
 import fs from "fs";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { exec, spawn } from 'child_process';
+import path from "path";
+import { fileURLToPath } from "url";
+import { exec } from "child_process";
 
 
-(async () => {
-    const argv = process.argv;
-    const hook_name = argv[2];
+(async function main () {
+    // 获取路径参数
+    const hook_name = process.argv[2];
+    console.debug(`当前正在执行 ${hook_name} 钩子`);
 
-    if (!["pre-commit", "commit-msg"].includes(hook_name)) {
-        console.warn(`Sorry, there is not the ${hook_name} hook.`)
-        process.exit(1);
-    } else {
-        console.log(`Starting ${hook_name} hook...`)
-    }
-
+    // 配置文件地址
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
+    console.debug("当前脚本所在文件夹", __dirname);
 
+    // 获取配置信息
     const configPath = path.join(__dirname, "..", ".samoyedrc.json");
+    const configData = JSON.parse(fs.readFileSync(configPath, "utf8"));
+    console.debug("成功获取萨摩耶配置信息", configData);
 
-    const configData = JSON.parse(fs.readFileSync(configPath, 'utf8'));
-
-    const hookConfigData = configData[hook_name]
-    console.log(hookConfigData)
-
-    // exec('git status -s')
-
-    const tmpContent = ""
+    // 获取当前钩子的配置
+    const hookConfig = configData[hook_name];
+    console.debug(`当前钩子的配置信息为`, hookConfig);
  
-    exec('git status -s', { windowsHide: true }, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`执行的错误: ${error}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`shell错误: ${stderr}`);
-            return;
-        }
-        const stashFileList = stdout.split("\n").map((file) => file.slice(3));
-        stashFileList.forEach((file) => {
-            console.log(file);
-        })
-        const hookFiltered = hookConfigData.map((hookConfig) => {
-            for (const stashFile of stashFileList) {
-                if (new RegExp(hookConfig.scope).test(stashFile)) {
-                    console.log("匹配成功", hookConfig.scope, stashFile)
-                    return hookConfig.commands;
-                }
+    // 获取暂存区文件信息
+    const stashFileList = (await new Promise((resolve, reject) => {
+        exec("git status -s", { windowsHide: true }, (error, stdout, stderr) => {
+            if (error) reject(error);
+            if (stdout) resolve(stdout);
+            if (stderr) resolve(stderr);
+        });
+    })).split("\n").map((_) => _.slice(3));
+    console.debug("暂存区文件信息", stashFileList);
+
+    // 匹配钩子作用域
+    const commands = [];
+    for (const config of hookConfig) {
+        const scopeExp = new RegExp(config.scope);
+        for (const stashFile of stashFileList) {
+            if (scopeExp.test(stashFile)) {
+                config.commands.forEach((_) => commands.push(_));
+                break;
             }
-            return []
-        })
-        console.log("pipei")
-        console.log(`匹配`, hookFiltered)
+        }
+    }
+    console.debug("作用域匹配的全部命令", commands);
 
-        // flatten
-        const commands = [];
-        hookFiltered.forEach((hooks) => {
-            hooks.forEach(cmd => commands.push(cmd));
-        })
-        console.log(commands);
+    // 执行全部命令
+    const promiseList = [];
+    for (const command of commands) {
+        promiseList.push(new Promise((resolve, reject) => {
+            exec(command, { windowsHide: true }, (error, stdout, stderr) => {
+                if (error) reject(error);
+                if (stdout) {
+                    console.log(stdout);
+                    resolve(stdout);
+                };
+                if (stderr) resolve(stderr);
+            });
+        }));
+    }
 
-        fs.writeFileSync(path.join(__dirname, 'cmd_tmp'), commands.join("\n"))
-
-        const cmd_tmp = fs.readFileSync(path.join(__dirname, 'cmd_tmp'), 'utf-8');
-        let commands_run = cmd_tmp.split("\n");
-        commands_run.forEach((cmd) => {
-            console.log("zhixing", cmd)
-            exec(cmd, { windowsHide: true }, (error, stdout, stderr) => {
-                if (error) console.log(error);
-                if (stdout) console.log(stdout)
-                if (stderr) console.log(stderr)
-            })
-        })
-
-        // process.exit(1)
+    // 获取命令执行结果
+    Promise.all(promiseList).then((values) => {
+        console.debug(values);
+        process.exit(0);
+    }, (reason) => {
+        console.error(reason);
+        process.exit(1);
     });
 })()
