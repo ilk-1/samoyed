c261e853904c84db2866a4d3a14aec68c0a3f248
ilk-1 <ilovekfc1024@qq.com>
2024-07-06 11:06:48 +0800

    test: 6

--- .samoyed/main.mjs
+++ .samoyed/main.mjs
@@ -7,21 +7,21 @@ import { exec } from "child_process";
 (async function main () {
     // 获取路径参数
     const hook_name = process.argv[2];
-    console.debug(`当前正在执行 ${hook_name} 钩子`);
+    // console.debug(`当前正在执行 ${hook_name} 钩子`);
 
     // 配置文件地址
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    console.debug("当前脚本所在文件夹", __dirname);
+    // console.debug("当前脚本所在文件夹", __dirname);
 
     // 获取配置信息
     const configPath = path.join(__dirname, "..", ".samoyedrc.json");
     const configData = JSON.parse(fs.readFileSync(configPath, "utf8"));
-    console.debug("成功获取萨摩耶配置信息", configData);
+    // console.debug("成功获取萨摩耶配置信息", configData);
 
     // 获取当前钩子的配置
     const hookConfig = configData[hook_name];
-    console.debug(`当前钩子的配置信息为`, hookConfig);
+    // console.debug(`当前钩子的配置信息为`, hookConfig);
  
     // 获取暂存区文件信息
     const stashFileList = (await new Promise((resolve, reject) => {
@@ -31,7 +31,7 @@ import { exec } from "child_process";
             if (stderr) resolve(stderr);
         });
     })).split("\n").map((_) => _.slice(3));
-    console.debug("暂存区文件信息", stashFileList);
+    // console.debug("暂存区文件信息", stashFileList);
 
     // 匹配钩子作用域
     const commands = [];
@@ -44,7 +44,7 @@ import { exec } from "child_process";
             }
         }
     }
-    console.debug("作用域匹配的全部命令", commands);
+    // console.debug("作用域匹配的全部命令", commands);
 
     // 执行全部命令
     const promiseList = [];
@@ -63,7 +63,7 @@ import { exec } from "child_process";
 
     // 获取命令执行结果
     Promise.all(promiseList).then((values) => {
-        console.debug(values);
+        // console.debug(values);
         process.exit(0);
     }, (reason) => {
         console.error(reason);
